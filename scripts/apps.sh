96d7bfc2f65ced74832b40cac5567c9315d18db1
MIT4893-Projects <minhngoc2891211@gmail.com>
2024-02-04 18:45:19 +0700

    update

--- scripts/apps.sh
+++ scripts/apps.sh
@@ -3,6 +3,7 @@ sudo pacman -S polkit-gnome \
 	dbeaver \
 	rofi \
 	nautilus \
+	gnome-calculator \
 	gnome-terminal \
 	gnome-system-monitor \
 	gnome-disk-utility \
@@ -16,14 +17,20 @@ sudo pacman -S polkit-gnome \
 	pamixer \
 	blueman \
 	bluez-utils \
-	ntfs-3g
+	ntfs-3g \
+	ibus \
+	npm \
+	wev \
+	networkmanager-openvpn
 
 yay -S visual-studio-code-bin \
 	microsoft-edge-stable-bin \
 	ookla-speedtest-bin \
 	pycharm-professional \
 	spotify \
-	kuro
+	kuro \
+	ibus-bamboo \
+	grimblast
 
 brew install neovim \
 	oh-my-posh
@@ -32,3 +39,6 @@ brew install neovim \
 
 # oh-my-posh
 echo 'eval "$(oh-my-posh init bash --config ~/.config/oh-my-posh/M365PrincessBased.omp.json)"' >>~/.bashrc
+
+# pip
+sudo rm /usr/lib/python3.11/EXTERNALLY-MANAGED
