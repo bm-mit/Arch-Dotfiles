92ad22e3a64914630737a6513b9f990e73ab9303
MIT4893-Projects <minhngoc2891211@gmail.com>
2024-02-05 10:12:56 +0700

    update scripts

--- scripts/apps.sh
+++ scripts/apps.sh
@@ -34,6 +34,22 @@ yay -S visual-studio-code-bin \
 brew install neovim \
 	oh-my-posh
 
+# Waybar install
+git clone https://github.com/Alexays/Waybar ~/git-clone/waybar
+cd ~/git-clone/Waybar
+sudo meson build
+sudo ninja -C build install
+cd -
+
+# Dragon (dragger support for X and Wayland)
+git clone https://github.com/DreamMaoMao/dragon-hyprland ~/git-clone/dragon-hyprland
+cd ~/git-clone/dragon-hyprland
+sudo make install
+cd -
+
+# cleanup cloned repos
+rm -rf ~/git-clone/
+
 # install ibus-bamboo
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/BambooEngine/ibus-bamboo/master/archlinux/install.sh)"
 
--- scripts/waybar.sh
+++ scripts/waybar.sh
@@ -1,9 +1,4 @@
 sudo pacman -S iw bc
 
-git clone https://github.com/Alexays/Waybar
-cd Waybar
-sudo meson build
-sudo ninja -C build install
-
 cd ..
 rm -rf Waybar
